63e8dcac2cff1c2ee01b8128afc9d7eb46396382
Dusk <65917000+duskdev17@users.noreply.github.com>
2022-09-28 23:41:06 +0600

    .

---
+++ login.php
@@ -0,0 +1,27 @@
+<?php include_once "header.php"; ?>
+<body>
+  <div class="wrapper">
+    <section class="form login">
+      <header>Royal Chat Web</header>
+      <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
+        <div class="error-text"></div>
+        <div class="field input">
+          <label>Email Address</label>
+          <input type="text" name="email" placeholder="Enter your email" required>
+        </div>
+        <div class="field input">
+          <label>Password</label>
+          <input type="password" name="password" placeholder="Enter your password" required>
+          <i class="fas fa-eye"></i>
+        </div>
+        <div class="field button">
+          <input type="submit" name="submit" value="Continue to Chat">
+        </div>
+      </form>
+      <div class="link">Not yet signed up? <a href="index.php">Signup now</a></div>
+    </section>
+  </div>
+
+
+</body>
+</html>
