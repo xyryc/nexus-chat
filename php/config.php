7e979b057037886ba6682c1bb1e7bc37496c5730
tamanna111 <91381507+tamanna111@users.noreply.github.com>
2022-10-13 11:19:11 +0600

    Update config.php

--- php/config.php
+++ php/config.php
@@ -5,6 +5,7 @@
   $dbname = "webchatapp";
 
   $conn = mysqli_connect($hostname, $username, $password, $dbname);
+  
   if(!$conn){
     echo "Database connection error".mysqli_connect_error();
   }
